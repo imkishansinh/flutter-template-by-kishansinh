b396b881542e27655d0dc6de1b7a054f5fd3ef80
Kishansinh Parmar <im@kishansinh.com>
2024-01-07 16:20:38 +0530

    monarch story updated

--- weather_app_by_kishansinh/stories/current_weather_details_widget_stories.dart
+++ weather_app_by_kishansinh/stories/current_weather_details_widget_stories.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app_by_kishansinh/features/weather_home/presentation/widgets/current_weather_details_widget.dart';
 
-Widget currentWeatherDetailsWidget() => const CurrentWeatherDetailsWidget(
-      humanDate: 'Now',
-      temp: 28,
-      iconUrl: 'https://openweathermap.org/img/wn/10d@2x.png?apiid=',
-      tempMax: 29,
-      tempMin: 19,
-      feelsLike: 28,
-      main: 'Smoke',
+Widget currentWeatherDetailsWidget() => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.purple,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      home: const Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 21),
+                child: CurrentWeatherDetailsWidget(
+                  humanDate: 'Now',
+                  temp: 28,
+                  iconUrl:
+                      'https://openweathermap.org/img/wn/10d@2x.png?apiid=1b621b7b5aad7d157e415fdb49953fa9',
+                  tempMax: 29,
+                  tempMin: 19,
+                  feelsLike: 28,
+                  main: 'Smoke',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
